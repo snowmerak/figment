271e07e76c4daf9aeae463196fbe52ad3678d7cf
snowmerak <snowmerak@outlook.com>
2022-04-11 14:54:06 +0900

    add server

--- exe/gateway/main.go
+++ exe/gateway/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	server := http.Server{
+		Addr: ":11211",
+	}
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
